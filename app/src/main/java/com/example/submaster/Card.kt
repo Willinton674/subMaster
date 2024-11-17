d6d243fe285675279dc6a034207a2317545baad2
Willinton-Ma <willinton.machado253@pascualbravo.edu.co>
2024-11-17 17:36:24 -0500

    Primer commit: Subida inicial del proyecto

---
+++ app/src/main/java/com/example/submaster/Card.kt
@@ -0,0 +1,11 @@
+package com.example.submaster
+
+data class Card (
+    val nombre:String,
+    val fecha_A:String,
+    val fecha_C:String,
+    val frecuencia:String,
+    val tipoBebito:String,
+    val tipo_ser:String,
+    val tarjeta:String
+)
