0fac5a1c47bc2fa7ea989fa35df7d0cc1823f86c
JuanCG437 <tatumenes240@gmail.com>
2024-11-19 20:45:12 -0500

    Class Service, Activity_Formulario
    Se agregó la clase "Service" con su respectivo Constructor, además se añadieron elementos en el Activity_Formulario para capturar los datos del diseño XML y pasarlos a variables.

--- app/src/main/java/com/example/submaster/Activity_Formulario.kt
+++ app/src/main/java/com/example/submaster/Activity_Formulario.kt
@@ -1,20 +1,80 @@
 package com.example.submaster
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class Activity_Formulario : AppCompatActivity() {
+
+    val editnombre: EditText = findViewById(R.id.txtNombre)
+    val editfecha_A: EditText = findViewById(R.id.txtFecha_A)
+    val editfecha_C: EditText = findViewById(R.id.txtFecha_B)
+    val editfrecuencia: EditText = findViewById(R.id.txtCobro)
+    val spinnertipoDebito: Spinner = findViewById(R.id.TipoDebito)
+    val radiotipo_ser: RadioGroup = findViewById(R.id.rdbTiposer)
+    val editTarjeta: EditText = findViewById(R.id.txtTarjeta)
+
+    val boton_aceptar: Button = findViewById(R.id.btnAgregar)
+    val boton_cancelar: Button = findViewById(R.id.btnCancelar)
+
+    // Configurar opciones del Spinner
+    // val opciones = listOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_formulario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        boton_aceptar.setOnClickListener(View.OnClickListener {
+            val nombre = editnombre.text.toString().trim()
+            val fecha_a = editfecha_A.text.toString().trim()
+            val fecha_c = editfecha_C.text.toString().trim()
+            val frecuencia = editfrecuencia.text.toString().trim()
+            // val tipodebito = spinnertipoDebito.selectedItem.toString()
+            val tipo_ser_id = radiotipo_ser.checkedRadioButtonId
+            val tiposer = if (tipo_ser_id != -1) {
+                findViewById<RadioButton>(tipo_ser_id).text.toString()
+            } else {
+                "" // Si ningún valor del radiobutton fue seleccionado devuelve cadena vacia
+            }
+            val tarjeta = editTarjeta.text.toString().trim()
+
+            if (nombre.isEmpty() ||
+                !esFechaValida(fecha_a) ||
+                !esFechaValida(fecha_c) ||
+                frecuencia.isEmpty() ||
+                tiposer.isEmpty() ||
+                tarjeta.isEmpty()){
+
+                Toast.makeText(this, "Completa todos los campos.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Todos los campos fueron completados.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    fun esFechaValida(fecha:String): Boolean {
+        return try {
+            val formato = SimpleDateFormat("dd/mm/yyy", Locale.getDefault())
+            formato.isLenient = false
+            formato.parse(fecha) != null
+        } catch (e: Exception) {
+            false
         }
     }
+
+
 }
---
+++ app/src/main/java/com/example/submaster/Service.kt
@@ -0,0 +1,9 @@
+package com.example.submaster
+
+import java.util.Date
+
+class Service (val nomre: String, val fecha_A: Date, val fecha_C: Date,
+               val frecuencia: String, val tipoDebito: String, val tipo_ser: String, val tarjeta: String){
+
+
+}
